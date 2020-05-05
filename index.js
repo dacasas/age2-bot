5f9e3b9708ea044b852e7aa107604db590d0d637
Diego Casas <dacasas@uc.cl>
2020-05-04 18:03:49 -0700

    Age 2 discord bot

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+// This is the token of the bot "Ha llegado Wallace"
+const token = 'NzA2MjM0OTI3MDQ5NDc0MTEw.Xq3Xxg.fpktfjCmpjW6_4YKBjzeEHCF8vg';
+
+const path = './sounds/';
+const extension = '.mp3';
+
+const sayNumber = (voiceChannel, message) => {
+	voiceChannel.join()
+		.then(connection => connection.play(path + message.content + extension))
+		.catch(error => console.log(`ERROR CAPTURADO: ${error}`));
+};
+
+const checkChannelConnection = (voiceChannel, message) => {
+	if (!voiceChannel) {
+		message.channel.send("Tienes que estar en un canal de voz");
+		return false;
+    }
+	return true;
+};
+
+const checkNumber = message => +message.content && +message.content < 43;
+
+bot.on('message', message => {
+	if (checkNumber(message)) {
+		const voiceChannel = message.member.voice.channel;
+		if (checkChannelConnection(voiceChannel, message)) {
+			sayNumber(voiceChannel, message);
+		}
+	}
+});
+
+bot.login(token);
