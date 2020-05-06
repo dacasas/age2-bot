19b709dd35519e326ec798b56a4678ea77e57202
unknown <miguel_z94@hotmail.com>
2020-05-05 22:11:02 -0400

    ajsakjs

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const L4D2_INFECTED = {
 // This is the token of the bot "Ha llegado Wallace"
 const token = process.env.BOT_AGE_TOKEN;
 
-const handleAge2Message = (voiceChannel, content) => {
+const handleAge2Message = (voiceChannel, content, directory) => {
   voiceChannel.join()
-		.then(connection => connection.play(`./sounds/age2/${content}.mp3`))
+		.then(connection => connection.play(`./sounds/${directory}/${content}.mp3`))
 		.catch(error => console.log(`ERROR EN ON PLAY: ${error}`));
 };
 
@@ -34,6 +34,7 @@ const handleL4d2Message = (voiceChannel, content) => {
 
 const l4d2Message = content => content.match(/([a-zA-Z]*)/);
 const age2Message = content => isNumeric(content);
+const randomGamesMessage = content => content.starswith('0') && isNumeric(content.substring(1));
 
 const isNumeric = content => !isNaN(content);
 
@@ -42,7 +43,8 @@ bot.on('message', message => {
 	if (isNumeric(content) || l4d2Message(content)) {
     const voiceChannel = message.member.voice.channel;
     if (voiceChannel) {
-      if (age2Message(content)) handleAge2Message(voiceChannel, content);
+      if (age2Message(content)) handleAge2Message(voiceChannel, content, 'age');
+      if (age2Message(content)) handleAge2Message(voiceChannel, content, 'games');
       if (l4d2Message(content)) handleL4d2Message(voiceChannel, content);
     } else  {
       message.channel.send("Tienes que estar en un canal de voz");
