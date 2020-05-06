6d6167fde8b4d1522c96b6efb3af35192c4b51e0
Diego Casas <dacasas@uc.cl>
2020-05-05 18:06:15 -0700

    Let users write only infected names

--- index.js
+++ index.js
@@ -19,23 +19,20 @@ const handleAge2Message = (voiceChannel, content) => {
   voiceChannel.join()
 		.then(connection => connection.play(`./sounds/age2/${content}.mp3`))
 		.catch(error => console.log(`ERROR EN ON PLAY: ${error}`));
-}
+};
 
 const handleL4d2Message = (voiceChannel, content) => {
-  const infectedName = l4d2Message(content)[1];
+  const infectedName = l4d2Message(content)[0];
   const numberOfSounds = L4D2_INFECTED[infectedName];
-  console.log(infectedName);
-  console.log(numberOfSounds);
   if (numberOfSounds) {
     const number = Math.ceil(Math.random() * Math.floor(numberOfSounds));
-    console.log(number);
     voiceChannel.join()
       .then(connection => connection.play(`./sounds/l4d2/${infectedName}/${number}.wav`))
       .catch(error => console.log(`ERROR EN ON PLAY: ${error}`));
   }
 };
 
-const l4d2Message = content => content.match(/-l4d2 ([a-zA-Z]*)/);
+const l4d2Message = content => content.match(/([a-zA-Z]*)/);
 const age2Message = content => isNumeric(content);
 
 const isNumeric = content => !isNaN(content);
